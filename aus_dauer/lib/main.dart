9c860b1882e39d3e3a0405cf8a483193e8b0f569
iskandarmrp <iskandarmudarp@gmail.com>
2024-01-11 22:52:42 +0700

    feat/add_product

--- aus_dauer/lib/main.dart
+++ aus_dauer/lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:aus_dauer/app/marketplace/marketplace.dart';
 import 'package:aus_dauer/app/manage_product/manage_product.dart';
+import 'package:aus_dauer/app/manage_product/add_product.dart';
 
 void main() {
-  runApp(const ManageProductPage());
+  runApp(const AddProductPage());
 }
